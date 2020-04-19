b9a4e416c25574778e2f271c7b2cc93d40fdaaea
yoshiyuki.K <lone.moon.signal@gmail.com>
2020-04-19 11:52:07 +0000

    20200419 fixed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,8 +12,8 @@ class TasksController < ApplicationController
   end
 
   def create
+
     @task = Task.new(task_params)
-    
     if @task.save
       flash[:success] = 'Task が正常に登録されました'
       redirect_to @task
--- app/models/talklist.rb
+++
@@ -1,2 +0,0 @@
-class Talklist < ApplicationRecord
-end
